c422c0a4e59009f8192d1c828ba77af797bbac27
Elisa <elisa.leroy@le-campus-numerique.fr>
2024-10-03 11:08:36 +0200

    création classe EquipementOffensif avec 3 constructeurs

---
+++ src/EquipementOffensif.java
@@ -0,0 +1,10 @@
+public class EquipementOffensif {
+
+    public EquipementOffensif(String item, int niveauAttaque, String name){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
