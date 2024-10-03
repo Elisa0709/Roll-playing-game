2d2848c5e00075187f1fd87119295bb563c3f7ce
Elisa <elisa.leroy@le-campus-numerique.fr>
2024-10-03 11:01:44 +0200

    création classe personnage avec 3 constructeurs

---
+++ src/Personnage.java
@@ -0,0 +1,17 @@
+public class Personnage {
+    public Personnage(){
+
+    }
+    public Personnage(String name){
+
+    }
+    public Personnage(String name, String type){
+
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
+}
